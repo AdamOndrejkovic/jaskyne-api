9ff38ca1ad125ab8ecacf5692565ed695eb16486
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2022-05-26 19:19:53 +0200

    fix

--- src/infrastructure/mongo/mongodb.provider.ts
+++ src/infrastructure/mongo/mongodb.provider.ts
@@ -1,10 +1,11 @@
 import * as mongoose from 'mongoose';
 import { MONGO_DATABASE_CONNECTION } from '../../common/cave.constants';
+import { ConfigService } from '@nestjs/config';
 
 export const MongodbProvider = [
   {
     provide: MONGO_DATABASE_CONNECTION,
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+    useFactory: async (config: ConfigService): Promise<typeof mongoose> =>
+      mongoose.connect(config.get('MONGO_DB')),
   },
 ];
